298370c48a0932d8b575a04029b1b6629eea23ff
Anneta Wamono <annetawamono@yahoo.com.au>
2021-01-15 10:51:35 -0800

    Updates

---
+++ precache-manifest.4c70a2e044cf50a09b02a0771855f28a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326e0c90adb0c3fb7ea41282909c0fe5",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "1b7b4814d1d749b24e71",
+    "url": "/movie-search-app/static/css/main.5e27496a.chunk.css"
+  },
+  {
+    "revision": "1944fb973b4ea304cf64",
+    "url": "/movie-search-app/static/js/2.569d9d5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-search-app/static/js/2.569d9d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7b4814d1d749b24e71",
+    "url": "/movie-search-app/static/js/main.179f388c.chunk.js"
+  },
+  {
+    "revision": "93854698e18f7c10401e",
+    "url": "/movie-search-app/static/js/runtime-main.fb6de319.js"
+  }
+]);
